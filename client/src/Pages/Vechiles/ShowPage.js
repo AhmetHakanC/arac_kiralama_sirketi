1cb33a5c04ea74fe1df0ac1564a38fee088ed42c
Ahmet Hakan <21010708003@ogrenci.bartin.edu.tr>
2024-06-02 23:43:45 +0300

    Second Commit

---
+++ client/src/Pages/Vechiles/ShowPage.js
@@ -0,0 +1,165 @@
+import Layout from "../../Layouts/Layout";
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+
+export function ShowPage() {
+    const {vehiclesId} = useParams();
+    const [vehicle, setVehicle] = useState([]);
+    const [flag, setFlag] = useState(false);
+
+    const load = useCallback(() => {
+        axios.get(`http://127.0.0.1:8080/vehicle/${vehiclesId}`).then((response) => {
+            setVehicle(response.data[0]);
+            setFlag(true);
+        });
+    }, [vehiclesId]);
+
+    useEffect(() => {
+        if (vehiclesId) {
+            load();
+        }
+    }, [vehiclesId, load]);
+
+    const updateVechileState = () => {
+        axios.put(`http://127.0.0.1:8080/vehicle/update/${vehiclesId}`, {})
+            .then((response) => {
+                if (response.data.success) {
+                    alert(response.data.message);
+                    load();
+                }
+            })
+    }
+
+    const updateVehiclePlate = (newPlate) => {
+        axios.put(`http://127.0.0.1:8080/vehicle/updatePlate`, {
+            aracId : vehicle.id,
+            aracPlaka : newPlate
+        }).then((response) => {
+            if (response.data.success) {
+                alert(response.data.message);
+                load();
+            }
+        })
+    }
+
+    const updateVehiclePrice = (newPrice) => {
+        axios.put(`http://127.0.0.1:8080/vehicle/updateCost`, {
+            aracId : vehicle.id,
+            aracFiyat : newPrice
+        }).then((response) => {
+            if (response.data.success) {
+                alert(response.data.message);
+                load();
+            }
+        })
+    }
+
+    const handleButtonClick = () => {
+        const newPlate = prompt('Yeni araç plakasını girin:');
+        if (newPlate !== null) {
+            updateVehiclePlate(newPlate);
+        }
+    }
+
+    const handleButtonClick2 = () => {
+        const newPrice = prompt('Yeni araç fiyatı girin:');
+        if (newPrice !== null) {
+            updateVehiclePrice(newPrice);
+        }
+    }
+
+
+    if (!flag) {
+        return (
+            <Layout>
+                <div>Yükleniyor...</div>
+            </Layout>
+        );
+    }
+    return (
+        <Layout>
+            <div className="h-20 px-10">
+                <div className="h-full border-b border-gray-500 flex items-center justify-between">
+                    <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Araçlar</h2>
+                    <div className="flex space-x-4 items-center">
+                        <button
+                            onClick={handleButtonClick}
+                            className="bg-gray-900 flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-1 focus:outline-offset-1">
+                            Araç Plaka Güncelle
+                        </button>
+                        <button
+                            onClick={handleButtonClick2}
+                            className="bg-gray-900 flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-1 focus:outline-offset-1">
+                            Araç Fiyat Güncelle
+                        </button>
+                        <button
+                            onClick={() => {updateVechileState()}}
+                            className="bg-gray-900 flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-1 focus:outline-offset-1">
+                            Araç Durumu Güncelle
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full h-screen justify-center flex p-6">
+                <div className="w-2/5">
+                    <img src={'https://i.im.ge/2024/06/01/Ki8X0Y.car-1.md.png'} className="object-cover" alt={"Arac"}/>
+                </div>
+                <div className="w-3/5 flex flex-col space-y-2 p-4">
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Fiyat:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.fiyat}₺</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Marka:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.marka}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Model:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.model}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Yıl:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.yil}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Yakıt:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.yakit}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Vites:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.vites}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">KM:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.kilometre}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Renk:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.renk}</div>
+                    </div>
+
+                    <div className="w-full  flex mt-20 border-b border-[#e2e4eb]">
+                        <div className="w-1/5 text-lg font-semibold">Durum:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.durum ? 'Kiralanmış' : 'Müsait'}</div>
+                    </div>
+
+                    <div className="w-full flex mt-20">
+                        <div className="w-1/5 text-lg font-semibold">Plaka:</div>
+                        <div className="w-4/5 text-lg font-mono">{vehicle.plaka}</div>
+                    </div>
+
+                </div>
+            </div>
+
+        </Layout>
+    );
+}
---
+++ server/index.js
@@ -0,0 +1,199 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mysql from 'mysql2';
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ahmet052014',
+    database: 'rent_a_car',
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL server');
+});
+
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+})
+
+app.get('/vehicles', (req,res) => {
+    const sql = "CALL getVehicles()";
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        return res.json(data[0]);
+    });
+});
+app.get('/vehicle/:vehicleId', (req, res) => {
+    const vehicleId = req.params.vehicleId;
+    const sql = "CALL getVehicle(?)";
+    db.query(sql, [vehicleId], (err, data) => {
+        if (err) {
+            return res.json(err);
+        }
+        return res.json(data[0]);
+    });
+});
+app.post('/vehicle/add', (req,res) => {
+    const { aracMarka, aracModel, aracYil, aracRenk, aracImageUrl, aracPlaka, aracFiyat, aracYakit, aracVites, aracKilometre, aracDurum } = req.body;
+    const sql = "CALL addVehicle(?,?,?,?,?,?,?,?,?,?,?)";
+    db.query(sql, [aracMarka, aracModel, aracYil, aracRenk, aracImageUrl, aracPlaka, aracFiyat, aracYakit, aracVites, aracKilometre, aracDurum], (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.json({ success: false, message: 'Bu plakaya ait başka bir araç zaten var' });
+        }
+        return res.json({ success: true, message: 'Araç başarıyla eklendi' });
+    });
+});
+app.put('/vehicle/update/:vehicleId', (req,res) => {
+    const vehicleId = req.params.vehicleId;
+    const sql = "CALL updateVehicleState(?)";
+    db.query(sql, [vehicleId], (err, data) => {
+        if (err) {
+            return res.json({ success: false, message: err });
+        }
+        return res.json({ success: true, message: 'Araç başarıyla güncellendi' });
+    });
+});
+app.put('/vehicle/updatePlate', (req,res) => {
+    const { aracId, aracPlaka } = req.body;
+    const sql = "CALL updateVehiclePlate(?,?)";
+    db.query(sql, [aracId,aracPlaka], (err, data) => {
+        if (err) {
+            return res.json({ success: false, message: err });
+        }
+        return res.json({ success: true, message: 'Araç başarıyla güncellendi' });
+    });
+});
+app.put('/vehicle/updateCost', (req,res) => {
+    const { aracId, aracFiyat } = req.body;
+    const sql = "CALL updateVehicleCost(?,?)";
+    db.query(sql, [aracId,aracFiyat], (err, data) => {
+        if (err) {
+            return res.json({ success: false, message: err });
+        }
+        return res.json({ success: true, message: 'Araç başarıyla güncellendi' });
+    });
+});
+app.delete('/vehicle/delete/:vehicleId', (req,res) => {
+    const vehicleId = req.params.vehicleId;
+    const sql = "CALL deleteVehicle(?)";
+    db.query(sql, vehicleId, (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Araç başarıyla silindi' });
+    });
+});
+
+app.get('/employees', (req,res) => {
+    const sql = "CALL getEmployees()";
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        return res.json(data[0]);
+    });
+});
+app.post('/employee/add', (req,res) => {
+    const { personelAd, personelSoyad, personelKimlik, personelTelefon, personelEmail, personelPozisyon } = req.body;
+    const sql = "CALL addEmployee(?,?,?,?,?,?)";
+    db.query(sql, [personelAd, personelSoyad, personelKimlik, personelTelefon, personelEmail, personelPozisyon], (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Personel başarıyla eklendi' });
+    });
+});
+app.delete('/employee/delete/:employeeId', (req,res) => {
+    const employeeId = req.params.employeeId;
+    const sql = "CALL deleteEmployee(?)";
+    db.query(sql, employeeId, (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Personel başarıyla silindi' });
+    });
+});
+
+app.get('/customers', (req,res) => {
+    const sql = "CALL getCustomers()";
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        return res.json(data[0]);
+    });
+});
+app.post('/customer/add', (req,res) => {
+    const { musteriAd, musteriSoyad, musteriKimlik, musteriTelefon, musteriEmail } = req.body;
+    const sql = "CALL addCustomer(?,?,?,?,?)";
+    db.query(sql, [musteriAd, musteriSoyad, musteriKimlik, musteriTelefon, musteriEmail], (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Müşteri başarıyla eklendi' });
+    });
+});
+app.delete('/customer/delete/:customerId', (req,res) => {
+    const customerId = req.params.customerId;
+    const sql = "CALL deleteCustomer(?)";
+    db.query(sql, customerId, (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Müşteri başarıyla silindi' });
+    });
+});
+
+app.get('/transactions', (req,res) => {
+    const sql = "CALL getTransactions()";
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        return res.json(data[0]);
+    });
+});
+app.post('/transaction/add', (req,res) => {
+    const { aracPlaka, personelKimlik, musteriKimlik, kiralamaTarih, teslimTarih, odeme_tur } = req.body;
+    const sql = "CALL addTransaction(?,?,?,?,?,?)";
+    db.query(sql, [aracPlaka, personelKimlik, musteriKimlik, kiralamaTarih, teslimTarih, odeme_tur], (err, data) => {
+        if(err) {
+            return res.json({ success: false, message: 'Teslim tarihi kiralama tarihinden önce olamaz' });
+        }
+        return res.json({ success: true, message: 'Kiralama işlemi başarıyla eklendi' });
+    });
+});
+app.delete('/transaction/delete/:transactionId', (req,res) => {
+    const transactionId = req.params.transactionId;
+    const sql = "CALL deleteTransaction(?)";
+    db.query(sql, transactionId, (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Kiralama işlemi başarıyla silindi' });
+    });
+});
+
+app.get('/maintenances', (req,res) => {
+    const sql = "CALL getMaintenances()";
+    db.query(sql, (err, data) => {
+        if (err) throw err;
+        return res.json(data[0]);
+    });
+});
+app.post('/maintenance/add', (req,res) => {
+    const { aracPlaka, bakimTarihi, bakimAciklama, bakimFiyat } = req.body;
+    const sql = "CALL addMaintenance(?,?,?,?)";
+    db.query(sql, [aracPlaka, bakimTarihi, bakimAciklama, bakimFiyat], (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Bakım işlemi başarıyla eklendi' });
+    });
+});
+app.delete('/maintenance/delete/:maintenanceId', (req,res) => {
+    const maintenanceId = req.params.maintenanceId;
+    const sql = "CALL deleteMaintenance(?)";
+    db.query(sql, maintenanceId, (err, data) => {
+        if (err) throw err;
+        return res.json({ success: true, message: 'Bakım bilgisi başarıyla silindi' });
+    });
+});
+
+app.listen(8080,() => {
+    console.log("Connected to server")
+});
